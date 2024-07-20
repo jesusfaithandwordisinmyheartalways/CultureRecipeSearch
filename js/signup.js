af05207f2011ad3b7ae4dd007782d184a7afea0e
andrewjohnson <andrewjohnson9393@gmail.com>
2024-07-20 13:04:30 -0500

    ! changed html & js files

--- js/signup.js
+++ js/signup.js
@@ -9,139 +9,94 @@
 
 
 
- const Username = () => {
-        
-    let username = document.getElementById('username')
-    if(username.value.trim() == '') {
-        document.getElementById('username-error').innerHTML = 'Must Contain One Uppercase, One Special Character, Min 8 characters';
-        document.querySelector('.Username-Error-Container').style.visibility = 'visible';
-        return false;
-    }
-
-    if(username.value.match(/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,}$/)) {
-        document.getElementById('username-error').innerHTML = 'Your Username Has Been Accepted';
-        document.querySelector('.Username-Error-Container').style.marginLeft = "110px";
-
-        setTimeout(() => {
-            document.querySelector('.Username-Error-Container').style.visibility = 'hidden';
-
-        }, 3000)
-           
-    } else {
-        document.getElementById('username-error').textContent = 'Must Contain One Uppercase, One Special Character, Min 8 characters';
-        document.querySelector('.Username-Error-Container').style.marginLeft = "0px";
-        document.querySelector('.Username-Error-Container').style.visibility = 'visible';
-        return false;
-    }
-
-    
-    if(username.value.match( /\b(?:fuck|shit|bitch|asshole|dick|cunt|bastard|slut|whore|damn|hell)\b/i)) {
-        document.getElementById('username-error').innerHTML = 'Username Contains Profanity';
-        document.querySelector('.Username-Error-Container').style.marginLeft = "110px";   
-        return false
-    }
-    }
 
 
 
 
 
-    const UserAgeData  = () => {
-
-        let userAge = document.getElementById('userAge')
-
-        if(userAge.value.match(/^[0-9]+$/ )) {
-            document.querySelector('.UserAge-Error-Container').style.visibility = 'hidden';
-            document.getElementById('user-age-error').textContent = '' ;
-
-        } else {
-            document.querySelector('.UserAge-Error-Container').style.visibility = 'visible';
-            document.getElementById('user-age-error').textContent = 'Enter Age';
-            return false;
-        }
-
-
-        if(userAge.value.length > 3 || userAge.value  <= -1 || userAge.value == -0) {
-            document.getElementById('user-age-error').textContent = 'Try Again'; 
-            document.querySelector('.UserAge-Error-Container').style.visibility = 'visible';
-            return false;
-        } 
-
-
-        if(userAge.value > 130) {
-            document.getElementById('user-age-error').textContent = 'Invalid Age';
-            document.querySelector('.UserAge-Error-Container').style.visibility = 'visible';
-            return false;
-        }
-
-        if(userAge.value >=0 && userAge.value <= 10) {
-            document.getElementById('user-age-error').textContent = 'Kid Age';
-            document.querySelector('.UserAge-Error-Container').style.visibility = 'visible';
-
-            setTimeout(() => {
-                document.querySelector('.UserAge-Error-Container').style.visibility = 'hidden';
-
-            }, 1000)
-            return true;
-
-        }
-
-        if(userAge.value > 10 && userAge.value <= 130) {
-            document.getElementById('user-age-error').textContent = 'Age Accepted';
-            document.querySelector('.UserAge-Error-Container').style.visibility = 'visible';
-
-            setTimeout(() => {
-                document.querySelector('.UserAge-Error-Container').style.visibility = 'hidden';
-
-            }, 3000)
-
-                return true;
-        }
-
-
-        if(userAge.value == null) {
-            document.querySelector('.UserAge-Error-Container').style.visibility = 'visible';
-            document.getElementById('user-age-error').textContent = 'Enter Age';
-            return
-        }
-    }
-
-
-
-
-
-    const UserEmailData = () => {
-        let userEmail = document.getElementById('user-email');
-
-        if(userEmail.value.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)) {
-            document.getElementById('user-email-error').innerHTML = 'Email Accepted';
-            document.querySelector('.UserEmail-Error-Container').style.visibility = 'hidden';
-        } else {
-            document.getElementById('user-email-error').innerHTML = 'Invalid Email';
-            document.querySelector('.UserEmail-Error-Container').style.visibility = 'visible';
-            return false;
-
-        }
+const cookieAttributes = {
+    path: '/',
+    domain: '127.0.0.1',
+    sameSite: 'Strict',
+    secure: false,
+    httpOnly: false,
+    expires: 7
 }
 
 
-        const cookieAttributes = {
-                path: '/',
-                domain: '127.0.0.1/sign-up.html',
-                sameSite: 'Strict',
-                secure: false,
-                httpOnly: false,
-                expires: 7
+
+
+        const UserName = () => {
+            const username = document.getElementById('username').value;
+        const userRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,}$/
+        if(!userRegex.test(username)) {
+            alert('Invalid username, Must Contain One Uppercase, One Special Character, Min 8 characters ')
+            return;
+        }
+        }
+
+
+        const UserAge = () => {
+            const userAge = document.getElementById('useAge').value;
+            if(userAge <= 0 || userAge >= 130) {
+            alert('Invalid Age')
+            return;
+          }
+          return true;
+
+        }
+
+
+
+        const UserEmail = () => {
+            const userEmail = document.getElementById('user-email').value;
+            const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+            if(!emailRegex.test(userEmail)) {
+                alert('Invalid Email')
+                return;
+            }
+            return true;
+          
         }
 
 
 
 
+    document.getElementById('formSignUp').addEventListener('submit', (event) => {
+             event.preventDefault();
+          
+
+            let submitUsername = document.getElementById('username').value;
+            let submitAge = document.getElementById('userAge').value;
+            let submitEmail = document.getElementById('user-email').value;
+    
+            if(localStorage.getItem("cookiesAccepted") === "true") {
+            // Set cookies to expire at the end of the day
+              const now = new Date();
+              const endOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 59, 59);
+              const expires = endOfDay.toUTCString()
+    
+              setCookie('username', submitUsername, expires);
+              setCookie('age', submitAge, expires);
+              setCookie('email', submitEmail, expires);
+            }
+
+            window.location.href = "/login.html";
+    })
+
+   
+
+     
 
 
- /*--------------------SET THE COOKIES -------------*/
+
+
+
+
+ /*-----------------------------------SET THE COOKIES -----------------------------------*/
         function setCookie(name, value, expires) {
-            document.cookie = `${name}=${value};expires=${expires};path=/`;
+            document.cookie = `${name}=${value};expires=${cookieAttributes.expires};path=${cookieAttributes.path};domain=${cookieAttributes.domain};SameSite=${cookieAttributes.sameSite}`;
+
         }
       
 
@@ -156,27 +111,13 @@
         }
 
 
+
+
+
+
+    
         
-        document.getElementById('formSignUp').addEventListener('submit', (e) => {
-            e.preventDefault();
-
-            let submitUsername = document.getElementById('username').value;
-            let submitAge = document.getElementById('userAge').value;
-            let submitEmail = document.getElementById('user-email').value;
-
-            if(localStorage.getItem("cookiesAccepted") === "true") {
-            // Set cookies to expire at the end of the day
-              const now = new Date();
-              const endOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 59, 59);
-              const expires = endOfDay.toUTCString()
-
-              setCookie('username', submitUsername, expires);
-              setCookie('age', submitAge, expires);
-              setCookie('email', submitEmail, expires);
-            }
-
-              window.location.href = "/login.html";
-        })
+   
 
 
           
@@ -258,22 +199,29 @@
 /*------------------------------------------CHAT POP UP BOX  ---------------------------------------------*/ 
             const openChat = () => {
               document.getElementById('chat-popup').style.display = 'block';
+              document.getElementById('text-message').value = '';
             }
 
 
             const closeChat = () => {
                 document.getElementById('chat-popup').style.display = 'none';
+                document.getElementById('text-message').value = '';
 
             }
 
 
             const submitChat = (event) => {
                 event.preventDefault()
-                document.getElementById('text-message').value = '' ? alert('Your Message Is Empty') 
-                ? document.getElementById('text-message').value !== '' : alert('Your Feedback Has Been Submitted') :
-                alert('Please Enter A Message') ;
+                if( document.getElementById('text-message').value === '') {
+                    alert('Your Message Is Empty') 
+                }
 
-                
+                if( document.getElementById('text-message').value !== '') {
+                    alert('Your Feedback has been submitted') ;
+                    closeChat();
+                    
+                }
+
 
             }
           
